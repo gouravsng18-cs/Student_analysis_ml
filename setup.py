4c84a5385e2b52d75033f1bc36556aeac80da4d4
Gourav Singh <gouravsng18@gmail.com>
2026-01-31 15:58:36 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name= 'trial',
+    version= "0.0.1",
+    author= "Gourav",
+    author_email= "gouravsng18@gmail.com",
+    requires=["pandas","numpy","seaborn"]
+)
